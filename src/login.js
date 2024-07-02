ec46cedb51119dd99f2fcfaaa4d922ad0e369ce0
Abhishek Raj <105824150+abhishek141001@users.noreply.github.com>
2024-07-03 03:04:27 +0530

    Update login.js

--- src/login.js
+++ src/login.js
@@ -25,7 +25,7 @@ function Login({ onLogin }) {
   };
 
   return (
-    <FacebookProvider appId="392084953510635">
+    <FacebookProvider appId={process.env.FB_APP_ID}>
       <div className="flex flex-col items-center">
         <LoginButton
           scope="email,public_profile,pages_show_list,read_insights,pages_read_engagement"
